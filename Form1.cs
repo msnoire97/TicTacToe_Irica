431f2bb1e3ff8a6908ee5c39a0b28767c8bc95c1
msnoire97 <gabrielle.irica26@gmail.com>
2021-07-16 23:25:26 +0800

    programmed winning conditionss

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,8 @@ namespace TicTacToe_Irica
     public partial class Form1 : Form
     {
         bool player = true;
+        bool winner = false;
+        int turns = 0;
 
         public Form1()
         {
@@ -29,8 +31,38 @@ namespace TicTacToe_Irica
 
             player = !player;
             tic.Enabled = false;
+            turns++;
+            
+        }
+
+        private void Winner_Check ()
+        {
+            if ((A1.Text == A2.Text) || (A2.Text == A3.Text) || (A1.Enabled = false))
+                winner = true;
+            else if ((B1.Text == B2.Text) || (B2.Text == B3.Text) || (B1.Enabled = false))
+                winner = true;
+            else if ((C1.Text == C2.Text) || (C2.Text == C3.Text) || (C1.Enabled = false))
+                winner = true;
+
+            else if ((A1.Text == B1.Text) || (B1.Text == C1.Text) || (A1.Enabled = false))
+                winner = true;
+            else if ((A2.Text == B2.Text) || (B2.Text == C2.Text) || (A2.Enabled = false))
+                winner = true;
+            else if ((A3.Text == B3.Text) || (B3.Text == C3.Text) || (A3.Enabled = false))
+                winner = true;
+
+            else if ((A1.Text == B2.Text) || (B2.Text == C3.Text) || (A1.Enabled = false))
+                winner = true;
+            else if ((A3.Text == B2.Text) || (B2.Text == C1.Text) || (A3.Enabled = false))
+                winner = true;
 
 
             }
+
+
         }
+
+
+
     }
+}
