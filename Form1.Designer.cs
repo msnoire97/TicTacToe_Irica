bfea711ca41886fdc81ad45d6e766c6b082b5707
msnoire97 <gabrielle.irica26@gmail.com>
2021-07-16 23:09:43 +0800

    created and programmed buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,176 @@ namespace TicTacToe_Irica
         /// </summary>
         private void InitializeComponent()
         {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.Menu = new System.Windows.Forms.MenuStrip();
+            this.About = new System.Windows.Forms.ToolStripMenuItem();
+            this.New = new System.Windows.Forms.ToolStripMenuItem();
+            this.Help = new System.Windows.Forms.ToolStripMenuItem();
+            this.Menu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(18, 90);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(130, 130);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(146, 90);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(130, 130);
+            this.A2.TabIndex = 1;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(274, 90);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(130, 130);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(274, 218);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(130, 130);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(146, 218);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(130, 130);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(18, 218);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(130, 130);
+            this.B1.TabIndex = 3;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(274, 346);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(130, 130);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(146, 346);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(130, 130);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(18, 346);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(130, 130);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Menu
+            // 
+            this.Menu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.About,
+            this.New,
+            this.Help});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(423, 33);
+            this.Menu.TabIndex = 9;
+            this.Menu.Text = "menuStrip1";
+            // 
+            // About
+            // 
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(78, 29);
+            this.About.Text = "About";
+            // 
+            // New
+            // 
+            this.New.Name = "New";
+            this.New.Size = new System.Drawing.Size(114, 29);
+            this.New.Text = "New Game";
+            // 
+            // Help
+            // 
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(65, 29);
+            this.Help.Text = "Help";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 436);
+            this.ClientSize = new System.Drawing.Size(423, 496);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.Menu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Tic Tac Toe";
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C1;
+        public System.Windows.Forms.Button C2;
+        private System.Windows.Forms.MenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem About;
+        private System.Windows.Forms.ToolStripMenuItem New;
+        private System.Windows.Forms.ToolStripMenuItem Help;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,25 @@ namespace TicTacToe_Irica
 {
     public partial class Form1 : Form
     {
+        bool player = true;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button tic = (Button)sender;
+            if (player == true)
+                tic.Text = "X";
+            else
+                tic.Text = "O";
+
+            player = !player;
+            tic.Enabled = false;
+
+
+            }
+        }
     }
-}
